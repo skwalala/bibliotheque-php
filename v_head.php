89cbed3a0d7911b626490d937046a631293d672b
paul jacobs <jacobspaul@yahoo.com>
2018-03-23 18:05:12 +0100

    update v_head
    changement de l'inclusion de monStyle.css vers css/monStyle.css

--- v_head.php
+++ v_head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" href="mesStyles.css">
+    <link rel="stylesheet" href="css/mesStyles.css">
     <link rel="shortcut icon" type="image/x-icon" href="iconeweb.ico" />
 </head>
 <body>
