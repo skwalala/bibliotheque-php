855021da0c23aac6d2e43b90634e4238f63fa7a7
paul jacobs <jacobspaul@yahoo.com>
2018-04-25 23:14:57 +0200

    Auteur add opperationel

--- Auteur_add.php
+++ Auteur_add.php
@@ -1,7 +1,19 @@
 <?php
 include('index.php');
+include("connexion_bdd.php");
+if(isset($_POST["name"]) AND isset($_POST["nickname"]))
+{
+        $donnees['name']=$_POST["name"];
+        $donnees['nickname']=$_POST['nickname'];
+        $ma_requete_SQL="INSERT INTO AUTEUR (idAuteur, nomAuteur, prenomAuteur) VALUES (NULL,'".$donnees['name']."','".$donnees['nickname']."');";
+        print_r($ma_requete_SQL);
+        $bdd->exec($ma_requete_SQL);
+        header("Location: Auteur_show.php");
+}
+
 ?>
-<form action="." method="post">
+
+<form action="Auteur_add.php" method="post">
   <fieldset>
 	<label for="name"> nom :</label>
 	<input id="name" type ="text" name="name">
