20e9da533a9ab7c4a465ec5fb5fe79a4bf56fbb9
paul jacobs <jacobspaul@yahoo.com>
2018-05-11 11:45:11 +0200

    ajout de exemplaire_show

---
+++ Exemplaire_show.php
@@ -0,0 +1,45 @@
+<?php
+include("index.php");
+include("connexion_bdd.php");
+
+$r_exemplaire="SELECT noExemplaire, noOeuvre, etat ,dateAchat, prix
+from EXEMPLAIRE
+WHERE noOeuvre =".$_GET['idOeuvre']."
+ORDER BY noExemplaire;";
+$exemplaire = $bdd->query($r_exemplaire);
+$data = $exemplaire->fetchAll();
+
+?>
+
+<a href="Exemplaire_add.php">ajouter un Exemplaire</a>
+
+<div class="row">
+	<table border="2">
+		<caption>Recapitulatifs des Oeuvres</caption>
+			<thead>
+			<tr><th>id exemplaire</th><th>id oeuvre</th><th>etat</th><th>dateAchat</th><th>prix</th><th>operations</th></tr>
+			</thead>
+			<tbody>
+			<?php foreach ($data as $value): ?>
+				<tr><td>
+						<?php echo $value['noExemplaire']; ?>
+					</td><td>
+						<?php echo $value['noOeuvre']; ?>
+					</td><td>
+						<?php echo $value['etat']; ?>
+					</td><td>
+						<?php echo $value['dateAchat']; ?>
+					</td><td>
+						<?php echo $value['prix']; ?>
+					</td><td>
+						<a href="Exemplaire_edit.php?noExemplaire=<?= $value['noExemplaire'];?>&noOeuvre=<?=$value['noOeuvre'];?>&etat=<?=$value['etat']; ?>&dateAchat=<?= htmlentities($value['dateAchat']);?>&prix=<?=$value['prix']?>?>">modifier</a>
+                                                  <a href="Exemplaire_delete.php?id=<?= $value['noExemplaire'];?>&idOeuvre=<?= $value['noOeuvre']?>">supprimer    </a>
+
+					
+					</td>
+				</tr>
+			<?php endforeach; ?>
+			<tbody>
+	</table>
+</div>
+
