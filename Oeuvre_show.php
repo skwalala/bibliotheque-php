36b2c345346509979d0bb8f511e8b69389fba692
paul jacobs <jacobspaul@yahoo.com>
2018-05-24 05:25:37 +0200

    Dernier commit avant d'essayer de dormir

--- Oeuvre_show.php
+++ Oeuvre_show.php
@@ -2,11 +2,15 @@
 include("index.php");
 include("connexion_bdd.php");
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 $r_oeuvre="SELECT titre, noOeuvre, idAuteur,dateParution
 from OEUVRE
 ORDER BY titre;";
 $oeuvres = $bdd->query($r_oeuvre);
 $data = $oeuvres->fetchAll();
+
 
 ?>
 
@@ -16,10 +20,18 @@ $data = $oeuvres->fetchAll();
 	<table border="2">
 		<caption>Recapitulatifs des Oeuvres</caption>
 			<thead>
-			<tr><th>titre de l'oeuvre</th><th>identifiant auteur</th><th>date de parution</th><th>exemplaires</th><th>operations</th></tr>
+			<tr><th>titre de l'oeuvre</th><th>identifiant auteur</th><th>date de parution</th><th>nmb exemplaire</th><th>nmb exemplaire dispo</th><th>exemplaires</th><th>operations</th></tr>
 			</thead>
 			<tbody>
-			<?php foreach ($data as $value): ?>
+			<?php foreach ($data as $value):
+                $noOeuvre=$value['noOeuvre'];
+                $r_exemplaire="SELECT COUNT(*)
+                from EXEMPLAIRE
+                WHERE noOeuvre=$noOeuvre;";
+                $result = $bdd->query($r_exemplaire);
+                $nmbExemplaire = $result->fetch();
+			?>
+
 				<tr><td>
 						<?php echo $value['titre']; ?>
 					</td><td>
@@ -27,6 +39,10 @@ $data = $oeuvres->fetchAll();
 					</td><td>
 						<?php echo $value['dateParution']; ?>
 					</td><td>
+                        <?php echo $nmbExemplaire['COUNT(*)']; ?>
+                    </td><td>
+                        <?php //echo $value3['nmbExemplaireDispo']; ?>
+                    </td><td>
 						<a href="Exemplaire_show.php?idOeuvre=<?= $value['noOeuvre']; ?>">Gérer Exemplaires</a>
 					</td><td>
 						<a href="Oeuvre_edit.php?idAuteur=<?= $value['idAuteur'];?>&titre='<?= $value['titre'];?>'&date=<?= htmlentities($value['dateParution']); ?>&id=<?= $value['noOeuvre'];?>">modifier</a>
